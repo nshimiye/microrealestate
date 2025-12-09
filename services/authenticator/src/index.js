b485feac33573948144b38e89594947dc08a3584
Marcellin Nshimiyimana <nshimiye.tech@gmail.com>
2025-12-09 16:35:57 -0500

    feat: connect to dyanamo as part of service inti

--- services/authenticator/src/index.js
+++ services/authenticator/src/index.js
@@ -85,6 +85,7 @@ async function Main() {
     await service.init({
       name: 'Authenticator',
       useMongo: true,
+      useDynamo: true, // TODO use env variable
       useRedis: true,
       onStartUp
     });
